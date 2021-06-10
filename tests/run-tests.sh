c94ce4f5dbfb3954f53238beff048d6f0e2abcf1
bhavikapanara <panara.bhavika@gmail.com>
2021-06-10 17:53:59 +0530

    upadte

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -27,7 +27,7 @@ echo "Script location: ${BASEDIR}"
 cd $BASEDIR/../
 echo PWD=$PWD
 # python3 -m pytest --cov-report=xml --cov-report=term tests/
-python3 -m pytest --cov=ambianic --cov-report=xml --cov-report=term tests/
+python3 -m pytest --cov=./ --cov-report=xml --cov-report=term tests/
 # if -u command line argument is passed, submit code coverage report to codecov.io
 # parse command line arguments
 # if [ "$upload_codecov" = true ] ; then
