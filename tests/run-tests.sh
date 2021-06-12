3ce79c741eac847cc89a697bd096358a91cf0dd5
bhavikapanara <panara.bhavika@gmail.com>
2021-06-12 23:32:42 +0530

    update

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -35,10 +35,9 @@ python3 -m pytest --cov=./ --cov-report=xml --cov-report=term tests/
 #    codecov
 # fi
 
-echo "detect effective CPU architecture"
+
 # detect effective CPU architecture
 if [[ ${ARCH} == *"amd64"* ]]; then
-    echo "testing..................."
     bash <(curl -s https://codecov.io/bash)
 fi
 
