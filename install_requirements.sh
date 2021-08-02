1d4cebfb8533d986c0f433881a87db50e9b3c68f
bhavikapanara <panara.bhavika@gmail.com>
2021-08-02 16:23:53 +0530

    debug
    Signed-off-by: bhavikapanara <panara.bhavika@gmail.com>

--- install_requirements.sh
+++ install_requirements.sh
@@ -40,7 +40,7 @@ then
   pip3 install --force-reinstall https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_x86_64.whl
 else
   # pip3 install https://dl.google.com/coral/python/tflite_runtime-1.14.0-cp37-cp37m-linux_x86_64.whl
-  pip3 install --force-reinstall https://github.com/google-coral/pycoral/releases/download/v2.0.0/pycoral-2.0.0-cp37-cp37m-linux_x86_64.whl
+  pip3 install --force-reinstall https://github.com/google-coral/pycoral/releases/download/v1.0.1/tflite_runtime-2.5.0-cp37-cp37m-linux_x86_64.whl
 fi
 
 pip3 list
