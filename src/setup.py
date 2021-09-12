ec98f74dfe0e1bbd09f6eeea432e6e94614dba68
bhavikapanara <panara.bhavika@gmail.com>
2021-09-12 18:13:26 +0530

    fix: html error
    Signed-off-by: bhavikapanara <panara.bhavika@gmail.com>

--- src/setup.py
+++ src/setup.py
@@ -1,2 +1,10 @@
 from setuptools import setup
-setup()
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
