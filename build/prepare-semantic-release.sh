87f17517c65e9a221bf93ba4903164fcf6eb2be0
bhavika panara <panara.bhavika@gmail.com>
2021-10-03 23:56:30 +0530

    feat: fall detect pypi publishing (#32)
    * feat: add temp file
    
    Signed-off-by: bhavikapanara <panara.bhavika@gmail.com>
    
    * feat: fall detect
    
    Signed-off-by: bhavikapanara <panara.bhavika@gmail.com>
    
    * add setup files
    
    Signed-off-by: bhavikapanara <panara.bhavika@gmail.com>

---
+++ build/prepare-semantic-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# verbose mode
+set -x
+
+RELEASE_VERSION=$1
+a=( ${RELEASE_VERSION//./ } )
+MAJOR=${a[0]}
+MINOR=${a[1]}
+PATCH=${a[2]}
+echo "RELEASE_VERSION=$RELEASE_VERSION"
+echo "MAJOR=$MAJOR"
+echo "MINOR=$MAJOR.$MINOR"
+echo "PATCH=$PATCH"
+
+# update version info in the ambianic python package setup.cfg
+cp README.md src/
+cd src
+python3 setup.py setopt --command metadata --option version --set-value $RELEASE_VERSION
+
+# verify if version is updated in setup.cfg
+echo "verify if version is updated in setup.cfg"
+cat setup.cfg
